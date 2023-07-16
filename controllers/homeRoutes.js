adf0f0544652ab06adcacb56731aa3593ac9ae91
k-pineda <karenp9310@gmail.com>
2023-07-15 20:10:28 -0400

    fixed homeroutes to show homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,26 +2,31 @@ const router = require('express').Router();
 const { Book, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
+
+router.get('/', async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    // Get all projects and JOIN with user data
+    const bookData = await Book.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
     });
 
-    const users = userData.map((book) => book.get({ plain: true }));
+    // Serialize data so the template can read it
+    const books = bookData.map((book) => book.get({ plain: true }));
 
-    res.render('book', {
-      users,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      books, 
+      logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 router.get('/book/:id', async (req, res) => {
   try {
@@ -68,7 +73,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/homepage');
+    res.redirect('/profile');
     return;
   }
 
