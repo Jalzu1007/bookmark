b6d90b5a5946f46bc5880b6c60ed8a3af3a0a2e1
k-pineda <karenp9310@gmail.com>
2023-07-16 19:50:37 -0400

    fixed the login redirect to profile not homepage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const bookRoutes = require('./bookRoutes');
+const historyRoutes = require('./historyRoutes');
 
 router.use('/users', userRoutes);
 router.use('/books', bookRoutes);
+router.use('/history', historyRoutes);
 
 module.exports = router;
